544bbd976cc6523a571b3aa00e94b4849993ea01
Shivam Bhardwaj <105438019+Shivam-a0621@users.noreply.github.com>
2024-02-18 21:05:40 +0530

    extractor

---
+++ extracter.py
@@ -0,0 +1,11 @@
+import re
+class LinkExtractor:
+    def __init__(self, data_string):
+        self.data_string = data_string
+
+    def extract_links(self):
+        url_pattern = r'https?://\S+'
+        links = re.findall(url_pattern, self.data_string)
+        return links
+    
+    
