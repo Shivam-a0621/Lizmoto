e77df8911f274a862ac9259090971442effa3ab7
Shivam Bhardwaj <105438019+Shivam-a0621@users.noreply.github.com>
2024-02-19 21:07:27 +0530

    Update extracter.py

--- extracter.py
+++ extracter.py
@@ -4,8 +4,9 @@ class LinkExtractor:
         self.data_string = data_string
 
     def extract_links(self):
-        url_pattern = r'https?://\S+'
+        url_pattern = r'https?://\S+?(?=\s*\])'
         links = re.findall(url_pattern, self.data_string)
         return links
+    
     
     
